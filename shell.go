43cd8b30eb48fe851a112a999b142767d6a1ea5d
hackerhgl <hackerhgl@gmail.com>
2022-04-24 09:05:04 +0500

    Worker based logs working

--- shell.go
+++ shell.go
@@ -1,21 +1,58 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 	"os/exec"
 	"strconv"
 )
 
+
+
 func DummyShell(worker int, start int, end int)  {
+	var logFile *os.File 
+	initWorkerLogs(worker, logFile)
 	for i:=start; i<=end; i++ {
-		b, err := exec.Command("./scripts/echo.sh", strconv.Itoa(worker), strconv.Itoa(start)).Output()
+		cmd := exec.Command("./scripts/echo.sh", strconv.Itoa(worker), strconv.Itoa(i))
+
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			// return 0, err
+		}
+
+		err = cmd.Start()
 
 		if err != nil {
 			println("ERROR WORKER", worker, start, err.Error())
 			return
 		}
 
-		println("SUCCESS")
-		print(string(b))
 
+		in := bufio.NewScanner(stdout)
+
+		for in.Scan() {
+			log.Printf(in.Text()) // write each line to your log, or anything you need
+		}
+		if err := in.Err(); err != nil {
+			log.Printf("error: %s", err)
+		}
 	}
+	closeWorkerLogs(logFile)
+}
+
+
+func initWorkerLogs(worker int, logFile *os.File ) {
+	path := fmt.Sprintf("logs/worker-%d.log", worker)
+	logFile, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		println("Error: initWorkerLogs")
+		return
+    }
+	log.SetOutput(logFile)
+}
+
+func closeWorkerLogs( logFile *os.File) {
+	logFile.Close()
 }
