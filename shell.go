3a62e4942703b107d0cb3fdf1f202e9cad9018fd
hackerhgl <hackerhgl@gmail.com>
2022-04-24 08:34:57 +0500

    single shell command working

--- shell.go
+++ shell.go
@@ -7,6 +7,15 @@ import (
 
 func DummyShell(worker int, start int, end int)  {
 	for i:=start; i<=end; i++ {
-		exec.Command("./scripts/echo.sh", strconv.Itoa(worker), strconv.Itoa(start)).Output()
+		b, err := exec.Command("./scripts/echo.sh", strconv.Itoa(worker), strconv.Itoa(start)).Output()
+
+		if err != nil {
+			println("ERROR WORKER", worker, start, err.Error())
+			return
+		}
+
+		println("SUCCESS")
+		print(string(b))
+
 	}
 }
