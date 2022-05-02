aaf00c73a3cd6d832a28d502947973147de79d0f
hackerhgl <hackerhgl@gmail.com>
2022-05-02 23:57:55 +0500

    Updated credentials

--- constants.go
+++ constants.go
@@ -1,6 +1,6 @@
 package main
 
-// const PLAYLIST = "PLoSjAzdJQCyfsUatRxBnaes_4yz3-ONm-"
-const PLAYLIST = "PLoSjAzdJQCyfgkgOEzP6ZPJlKmb1w2FsD"
+const PLAYLIST = "PLoSjAzdJQCyfsUatRxBnaes_4yz3-ONm-" // Original playlist around 300+ items
+// const PLAYLIST = "PLoSjAzdJQCyfgkgOEzP6ZPJlKmb1w2FsD" // testing playlist ~12 items approximately
 const MAX_BATCHES = 4
 const MIN_PER_BATCH = 5
