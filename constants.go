8e5a4f4d29a03ffeda4421e4b8d7263f297ea507
hackerhgl <hackerhgl@gmail.com>
2022-07-25 06:48:18 +0500

    Production credentials

--- constants.go
+++ constants.go
@@ -1,10 +1,12 @@
 package main
 
-// const PLAYLIST = "PLoSjAzdJQCyfsUatRxBnaes_4yz3-ONm-" // Original playlist around 300+ items
-const PLAYLIST = "PLoSjAzdJQCyfgkgOEzP6ZPJlKmb1w2FsD" // testing playlist ~12 items approximately
+const PLAYLIST = "PLoSjAzdJQCyfsUatRxBnaes_4yz3-ONm-" // Original playlist around 300+ items
+// const PLAYLIST = "PLoSjAzdJQCyfgkgOEzP6ZPJlKmb1w2FsD" // testing playlist ~12 items approximately
 // const PLAYLIST = "PLoSjAzdJQCyfc54YG3EA6ew4EDXkYlDJe" // testing2
 const MAX_BATCHES = 4
 const MIN_PER_BATCH = 5
 
-// const DIR_NAME = "Music" // Production
-const DIR_NAME = "MusicDebug" // Debug
+const DIR_NAME = "Music" // Production
+// const DIR_NAME = "MusicDebug" // Debug
+
+// yt-playlist-sync@yt-playlist-sync-348101.iam.gserviceaccount.com
